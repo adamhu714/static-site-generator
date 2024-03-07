00d750b2c9f0db93e1f1e38379206ad8bac25f87
adamhu714 <adamhu714@gmail.com>
2024-03-07 21:00:54 +0000

    Added texnode tests

--- src/textnode.py
+++ src/textnode.py
@@ -1,10 +1,17 @@
+text_type_text = "text"
+text_type_bold = "bold"
+text_type_italic = "italic"
+text_type_code = "code"
+text_type_link = "link"
+text_type_image = "image"
+
 class TextNode:
     def __init__(self, text="", text_type="", url=None) -> None:
         self.text = text # The text content of the node
         self.text_type = text_type # Just a string like "bold" or "italic"
         self.url = url # The URL of the link or image, if the text is a link.
 
-    def eq(self, other) -> bool:
+    def __eq__(self, other) -> bool:
         return (self.text == other.text
             and self.text_type == other.text_type
             and self.url == other.url)
