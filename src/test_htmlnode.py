e23ae1f8e5b378b14bd1000a06316a3171a7bf35
adamhu714 <adamhu714@gmail.com>
2024-03-09 23:53:33 +0000

    Finished parent node

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode, LeafNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -26,12 +26,32 @@ class TestHTMLNode(unittest.TestCase):
 
     def test_to_html_no_children(self):
         node = LeafNode("p", "This is a value")
-        self.assertEqual("<p>This is a value</p>", node.to_html())
+        self.assertEqual(node.to_html(), "<p>This is a value</p>")
 
     def test_to_html_no_tag(self):
         node = LeafNode(None, "This is a value")
-        self.assertEqual("This is a value", node.to_html())
-
+        self.assertEqual(node.to_html(), "This is a value")
+    
+    def test_to_html_with_children(self):
+        child_node = LeafNode("p", "This is a child value")
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(parent_node.to_html(), '<div><p>This is a child value</p></div>')
+    
+    def test_to_html_with_grand_children(self):
+        grandchild_node = LeafNode("p", "This is a grandchild value")
+        child_node = ParentNode("div", [grandchild_node])
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(parent_node.to_html(), '<div><div><p>This is a grandchild value</p></div></div>')
+    
+    def test_to_html_with_many_children(self):
+        child_nodes = [
+            LeafNode("b", "Bold text"),
+            LeafNode(None, "Normal text"),
+            LeafNode("i", "italic text"),
+            LeafNode(None, "Normal text"),
+        ]
+        parent_node = ParentNode("p", child_nodes)
+        self.assertEqual(parent_node.to_html(), '<p><b>Bold text</b>Normal text<i>italic text</i>Normal text</p>')
 
 if __name__ == "__main__":
     unittest.main()
